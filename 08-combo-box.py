b247faf9f35b09d5ec6cdcbc5a35d881ac62b5ad
sanggon6107 <sanggon6107@naver.com>
2022-03-11 20:02:58 +0900

    combobox

---
+++ 08-combo-box.py
@@ -0,0 +1,20 @@
+import tkinter.ttk as ttk
+from tkinter import *
+
+root = Tk()
+root.title("combo-box")
+root.geometry("640x480+200+200")
+
+combo_values = [str(i) + "일" for i in range(1, 32)]
+combo_box = ttk.Combobox(root, height = 5, values = combo_values, state = "readonly" ) # readonly : 콤보박스에 타이핑 못하도록 한다.
+combo_box.pack()
+#combo_box.current(0)
+combo_box.set("카드 결제일")
+
+def btn_cmd() :
+    print(combo_box.get())
+
+btn = Button(root, text = "선택", command = btn_cmd)
+btn.pack()
+
+root.mainloop()
